182d406fb35577ce580cd43539119ae88c2cb448
Kugo Okabe <C0B220329e@edu.teu.ac.jp>
2022-06-23 17:37:15 +0900

    Co-authored-by: c0b22080ae <c0b22080ae@users.noreply.github.com> Co-authored-by: c0a2205071 <c0a2205071@users.noreply.github.com> Co-authored-by: fushimity <fushimity@users.noreply.github.com> Co-authored-by: c0a22026 <c0a22026@users.noreply.github.com>

---
+++ janken.py
@@ -0,0 +1,26 @@
+import random
+
+print("0~2整数を入力せよ[0:グー、1:チョキ、2:パー")
+
+for i in range(3):
+    you = int(input("You"))
+    cpu = random.randint(0,2)
+    print("CPU：", cpu, end=" ")
+
+    if  you == cpu:
+        print("Draw!")
+
+    elif (you + 1) %3 == cpu:
+        print("You Win!")
+
+    else:
+        print("You Lose!")
+
+
+
+
+
+
+
+
+
